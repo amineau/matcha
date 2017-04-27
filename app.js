ff50aaf48fdd0d85bbe21d850a20c95191d6809a
Antoine MINEAU <amineau@student.42.fr>
2017-04-27 10:28:08 +0200

    app

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ const session = require('express-session')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const conf = require('./config/conf')
+const MongoClient = require('mongodb').MongoClient
+const assert = require('assert')
+
+// Connection URL
+var url = 'mongodb://localhost:27017/matcha'
+// Use connect method to connect to the Server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server")
+  db.close()
+})
 
 let app = express()
 
